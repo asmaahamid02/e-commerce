1f33813a332ff004b7db92c768aafe0fb1bbb4ee
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-22 00:54:29 +0300

    EC-30 adding the php-backend editing the seller  info

--- ecommerce-server/api/delete_seller.php
+++ ecommerce-server/api/delete_seller.php
@@ -4,11 +4,13 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: X-Requested-With');
 header('Content-Type: application/json');
 include('connection.php');
-
+//
 if (isset($_POST['id'])) {
 
     $id = $_POST['id'];
     $user_type=$connection->prepare("DELETE  FROM userS WHERE id=$id ");
     $user_type->execute();
+
+    
 }
     ?>
