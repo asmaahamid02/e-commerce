d4fc5b353e61c216155affb2f6fd926b924eb244
Peter Saba <petersaba001@gmail.com>
2022-09-21 19:13:28 +0300

    EC-17 frontend: checking username format

--- admin-electron/src/javascript/add-seller.js
+++ admin-electron/src/javascript/add-seller.js
@@ -1,4 +1,10 @@
 const input_image = document.getElementById('image')
+const input_username = document.getElementById('username-input')
+const username_error = document.getElementById('username-error')
+const input_email = document.getElementById('email-input')
+const email_error = document.getElementById('email-error')
+const input_password = document.getElementById('password-input')
+const password_error = document.getElementById('password-error')
 
 input_image.onchange = function() {
     const reader = new FileReader()
@@ -9,4 +15,14 @@ input_image.onchange = function() {
     }
     console.log(reader.files)
     reader.readAsDataURL(this.files[0])
+}
+
+input_username.onblur = () =>{
+    const pattern = /^\w{2,}\w$/
+    const result = input_username.value.search(pattern)
+    if(result == -1){
+        username_error.innerText = '*should be at least 3 characters with no spaces'
+    }else{
+        username_error.innerText = ''
+    }
 }
