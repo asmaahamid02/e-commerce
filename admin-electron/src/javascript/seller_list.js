d0ce2e4a5b2036e88daf19e77b94662b357b3230
Peter Saba <petersaba001@gmail.com>
2022-09-22 18:59:34 +0300

    EC-42 link: adding seller form show feature

--- admin-electron/src/javascript/seller_list.js
+++ admin-electron/src/javascript/seller_list.js
@@ -1,9 +1,14 @@
 const remove= document.getElementById("remove")
 const tableUsername=document.getElementById("table-username")
+const add_button = document.getElementById("add-button")
 
 remove.addEventListener("click",()=>{
     remove.innerText="Removed"
 })
 tableUsername.addEventListener("click",()=>{
     window.location.replace("profile.html")
+})
+
+add_button.addEventListener("click", () =>{
+    console.log(parent.document.querySelector("body").querySelector(".background").style.visibility = "visible")
 })
