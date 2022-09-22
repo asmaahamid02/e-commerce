b526c9b008f388f5178e3999686ee950307ad306
Peter Saba <petersaba001@gmail.com>
2022-09-22 20:04:48 +0300

    EC-42 link: fixing bugs

--- admin-electron/src/javascript/seller_list.js
+++ admin-electron/src/javascript/seller_list.js
@@ -1,6 +1,7 @@
 const remove= document.getElementById("remove")
 const tableUsername=document.getElementById("table-username")
 const add_button = document.getElementById("add-button")
+const popup_element = parent.document.querySelector("body").querySelector(".background")
 
 remove.addEventListener("click",()=>{
     remove.innerText="Removed"
@@ -9,6 +10,9 @@ tableUsername.addEventListener("click",()=>{
     window.location.replace("profile.html")
 })
 
+// show popup for seller info and change buttons from edit to add
 add_button.addEventListener("click", () =>{
-    console.log(parent.document.querySelector("body").querySelector(".background").style.visibility = "visible")
+    popup_element.style.visibility = "visible"
+    popup_element.querySelector(".other-background").innerText = "ADD SELLER"
+    popup_element.querySelector(".green-background").innerText = "Add profile picture"
 })
