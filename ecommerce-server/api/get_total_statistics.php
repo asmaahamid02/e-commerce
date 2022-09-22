3dc86ad61bbf00715165a2ec20e11b7ad1a46056
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-22 10:43:01 +0300

    editing total statistics

--- ecommerce-server/api/get_total_statistics.php
+++ ecommerce-server/api/get_total_statistics.php
@@ -5,36 +5,32 @@ header('Access-Control-Allow-Headers: X-Requested-With');
 header('Content-Type: application/json');
 include('connection.php');
 /*get total sellers */
-$user_type=$connection->prepare("SELECT count(*) as total_sellers FROM seller");
+$id_seller=2;
+$id_client=1;
+$user_type=$connection->prepare("SELECT count(*) as total_sellers FROM users WHERE  type_id=$id_seller");
 $user_type->execute();
 $array=$user_type->get_result();
 $var=$array->fetch_assoc(); 
 $response=[];
-$response=$var;
-$total_user=$response;
-$json=json_encode($response);
-echo $json;
+$response[]=$var;
+
 
 /*get totalproducts */
 $user_type=$connection->prepare("SELECT count(*) as total_products FROM products");
 $user_type->execute();
 $array=$user_type->get_result();
 $var=$array->fetch_assoc(); 
-$response=[];
-$response=$var;
-$total_user=$response;
-$json=json_encode($response);
-echo $json;
+$response[]=$var;
+
+
 
 
 /*get totalclients */
-$user_type=$connection->prepare("SELECT count(*) as totalclients_ FROM client");
+$user_type=$connection->prepare("SELECT count(*) as totalclients_ FROM users WHERE type_id=$id_client");
 $user_type->execute();
 $array=$user_type->get_result();
 $var=$array->fetch_assoc(); 
-$response=[];
-$response=$var;
-$total_user=$response;
+$response[]=$var;
 $json=json_encode($response);
 echo $json;
 ?>
