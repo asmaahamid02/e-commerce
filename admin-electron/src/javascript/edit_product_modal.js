077579b1b3ef310a3f41a208bde387b72b3ef175
jessymatta <jessy.w.matta@gmail.com>
2022-09-22 16:36:49 +0300

    EC-53 frontend: finalizing the js script that closes and opens the edit profile modal

--- admin-electron/src/javascript/edit_product_modal.js
+++ admin-electron/src/javascript/edit_product_modal.js
@@ -1,1 +1,13 @@
 console.log("working?");
+
+const modalBtn = document.querySelector('.edit_product_modal_btn');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+
+modalBtn.addEventListener('click', function(){
+    modalBg.classList.add('bg-active');
+})
+
+modalClose.addEventListener('click', function(){
+    modalBg.classList.remove('bg-active');
+})
