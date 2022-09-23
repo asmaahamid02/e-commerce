aa5795e17b6252a8f3249c03a2be37bf8ddd5dbb
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-23 09:10:48 +0300

    EC-85 backend: adding constants used

--- ecommerce-server/api/add_discount.php
+++ ecommerce-server/api/add_discount.php
@@ -6,4 +6,9 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: X-Requested-With');
 header('Content-Type: application/json');
 
+$seller_id = $_POST["seller_id"];
+$code = $_POST["code"];
+$percentage = $_POST["percentage"];
+
+
 ?>
