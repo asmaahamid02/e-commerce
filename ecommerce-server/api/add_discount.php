fdd0c70be9a90bef448e9ac9749615fec0d257fc
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-23 09:07:38 +0300

    EC-85 backend: preparing php file

---
+++ ecommerce-server/api/add_discount.php
@@ -0,0 +1,9 @@
+<?php
+include('connection.php');
+include_once "common.php";
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: X-Requested-With');
+header('Content-Type: application/json');
+
+?>
