64c89423f977b5922b1f45753d8b4fa74955e649
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-23 09:29:09 +0300

    EC-85 backend: adding sql statement

--- ecommerce-server/api/add_discount.php
+++ ecommerce-server/api/add_discount.php
@@ -6,9 +6,25 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: X-Requested-With');
 header('Content-Type: application/json');
 
-$seller_id = $_POST["seller_id"];
-$code = $_POST["code"];
-$percentage = $_POST["percentage"];
+if (
+    isset ($_POST["seller_id"], $_POST["code"], $_POST["percentage"])
+    )
+{
+    $seller_id = $_POST["seller_id"];
+    $code = $_POST["code"];
+    $percentage = $_POST["percentage"];
+
+    $sql='INSERT INTO discounts(seller_id, code, percentage) VALUES(?,?,?)';
+
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param('i,s,d', $seller_id, $code, $percentage);
+
+ }//else {
+//     $response  = $common->getRepsonse('Not enough data submitted');
+// }
+
+
+
 
 
 ?>
