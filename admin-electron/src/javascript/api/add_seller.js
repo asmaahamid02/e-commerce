a0d1d7c7397536f1e0dcc5653b60f280afb3cd55
Peter Saba <petersaba001@gmail.com>
2022-09-23 11:12:10 +0300

    EC-42: linking to create seller api

--- admin-electron/src/javascript/api/add_seller.js
+++ admin-electron/src/javascript/api/add_seller.js
@@ -2,8 +2,19 @@ const submit = document.querySelector(".other-background")
 
 submit.addEventListener('click', () =>{
     if (validName() && validUsername() && validPass() && validEmail()){
-        console.log("good format")
+        const data = new FormData()
+        data.append('username', input_username.value)
+        data.append('password', input_password.value)
+        data.append('email', input_email.value)
+        data.append('type', 'seller')
+        data.append('name', input_name.value)
+        addSeller(data)
     }else{
         console.log("bad format")
     }
 })
+
+async function addSeller(data){
+    const response = await axios.post("http://localhost/e-commerce/ecommerce-server/api/create_user.php", data)
+    console.log(response.data)
+}
