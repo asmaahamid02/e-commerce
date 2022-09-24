500c0a3e76034276a2c75eb3fa9c83a111765f69
jessymatta <jessy.w.matta@gmail.com>
2022-09-24 14:22:08 +0300

    EC-76 frontend: adding media queries for the edit profile modal

--- client-frontend/assets/javascript/script.js
+++ client-frontend/assets/javascript/script.js
@@ -18,11 +18,14 @@ prevButton.addEventListener("click", () => {
 const modalBtn = document.getElementById('edit-profile-icon');
 const modalBg = document.querySelector('.modal-bg');
 const modalClose = document.querySelector('.modal-close');
-const modalSave=document.querySelector('.edit_profile_modal_save_btn')
+const modalSave=document.querySelector('.edit_profile_modal_save_btn');
+// const burgerMenu = document.querySelector('.menu');
 
 //When the button that should show the modal is clicked
 modalBtn.addEventListener('click', function(){
+
     modalBg.classList.add('bg-active');
+
 })
 
 //When the X at the top left of the modal is clicked
