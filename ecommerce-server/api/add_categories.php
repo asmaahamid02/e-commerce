fbf309fd93b95cacb48cfcebfc86aa83729c4f9a
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-09-24 22:46:48 +0300

    EC-124 backend: fixing categories api

--- ecommerce-server/api/add_categories.php
+++ ecommerce-server/api/add_categories.php
@@ -11,18 +11,29 @@ if (
     $category = $_POST["category"];
 
     if ($common->checkUser($seller_id)) {
-        $sql = 'INSERT INTO categories(seller_id, category, created_at) VALUES(?,?, NOW())';
-
+        $sql = 'select id from categories where category = ? and seller_id = ?';
         $stmt = $connection->prepare($sql);
-        $stmt->bind_param('is', $seller_id, $category);
-        $result = $stmt->execute() or die($connection->error);
-
-        if ($result) {
-            $response = $common->getRepsonse(1, null, 'Category added Successfully');
-        } else {
-            $response = $common->getRepsonse(0, null, 'Could not change!');
-        }
+        $stmt->bind_param('si', $category, $seller_id);
+        $stmt->execute() or die($connection->error);
+        $result = $stmt->get_result();
         $stmt->close();
+
+        if ($result->num_rows == 0) {
+            $sql = 'INSERT INTO categories(seller_id, category, created_at) VALUES(?,?, NOW())';
+
+            $stmt = $connection->prepare($sql);
+            $stmt->bind_param('is', $seller_id, $category);
+            $result = $stmt->execute() or die($connection->error);
+
+            if ($result) {
+                $response = $common->getRepsonse(1, null, 'Category added Successfully');
+            } else {
+                $response = $common->getRepsonse(0, null, 'Could not change!');
+            }
+            $stmt->close();
+        } else {
+            $response = $common->getRepsonse(0, null, 'Category already found');
+        }
     } else {
         $response = $common->getRepsonse(0, null, 'Seller not found');
     }
