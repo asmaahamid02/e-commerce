fdb67c6c6636434cdf10c2679e920bab6a6355cc
jessymatta <jessy.w.matta@gmail.com>
2022-09-25 13:08:52 +0300

    EC-131 frontend: liking the get_client_profile.php api to the edit profile modal

--- client-frontend/assets/javascript/script.js
+++ client-frontend/assets/javascript/script.js
@@ -18,22 +18,52 @@ prevButton.addEventListener("click", () => {
 const modalBtn = document.getElementById('edit-profile-icon');
 const modalBg = document.querySelector('.modal-bg');
 const modalClose = document.querySelector('.modal-close');
-const modalSave=document.querySelector('.edit_profile_modal_save_btn');
+const modalSave = document.querySelector('.edit_profile_modal_save_btn');
 // const burgerMenu = document.querySelector('.menu');
 
 //When the button that should show the modal is clicked
-modalBtn.addEventListener('click', function(){
+modalBtn.addEventListener('click', function () {
 
     modalBg.classList.add('bg-active');
 
 })
 
 //When the X at the top left of the modal is clicked
-modalClose.addEventListener('click', function(){
+modalClose.addEventListener('click', function () {
     modalBg.classList.remove('bg-active');
 })
 
 //When the Save button is clicked
-modalSave.addEventListener('click', function(){
+modalSave.addEventListener('click', function () {
     modalBg.classList.remove('bg-active');
 })
+
+
+//-------------------------------------------------------------------------------------------
+// Retrieving logged in users data and displaying it in the edit profile modal
+//! To remove later on
+const client_id = 2;
+//!should be const user = JSON.parse(localStorage.getItem("user"));
+//! const client_id=user.id;
+const client_name_input=document.getElementById('profile-input-name');
+const client_username_input=document.getElementById('profile-input-username');
+const client_email_input=document.getElementById('profile-input-email');
+
+
+async function getProfile() {
+    const response = await axios.get(`http://localhost/e-commerce/ecommerce-server/api/get_client_profile.php?client_id=${client_id}`).then(
+        (response) => {
+            console.log(response.data);
+            console.log(response.data.data[0]);
+            client_name_input.value=response.data.data[0].client_name;
+            client_username_input.value=response.data.data[0].client_username;
+            client_email_input.value=response.data.data[0].email;
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
+}
+
+// Function calls
+getProfile();
