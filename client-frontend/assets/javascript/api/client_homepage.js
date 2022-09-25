733ab2c997aa01c94ef626571008d3d9a92ca818
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-09-25 20:25:57 +0300

    removing duplicated

--- client-frontend/assets/javascript/api/client_homepage.js
+++ client-frontend/assets/javascript/api/client_homepage.js
@@ -1,6 +1,5 @@
 const cardsContainer = document.querySelector('.product_cards_container')
 const id = JSON.parse(localStorage.getItem('user')).id
-getAllProducts()
 
 async function getSomeProducts() {
   const response = await axios.get(
@@ -23,35 +22,6 @@ async function getAllProducts() {
   const response = await axios.get(
     'http://localhost/e-commerce/ecommerce-server/api/get_all_products.php?id=' +
       id
-  )
-  const data = response.data.data
-  for (const i of data) {
-    cardsContainer.appendChild(
-      createCard(i.title, i.description, i.price, 'dummy_product_pic.jpg', i.id)
-    )
-  }
-  return data
-}
-
-async function getSomeProducts() {
-  const response = await axios.get(
-    'http://localhost/e-commerce/ecommerce-server/api/get_all_products.php'
-  )
-  const data = response.data.data
-  let count = 0
-  for (const i of data) {
-    cardsContainer.appendChild(
-      createCard(i.title, i.description, i.price, 'dummy_product_pic.jpg', i.id)
-    )
-    count++
-    if (count == 8) return data
-  }
-  return data
-}
-
-async function getAllProducts() {
-  const response = await axios.get(
-    'http://localhost/e-commerce/ecommerce-server/api/get_all_products.php'
   )
   const data = response.data.data
   for (const i of data) {
@@ -61,3 +31,5 @@ async function getAllProducts() {
   }
   return data
 }
+
+getAllProducts()
