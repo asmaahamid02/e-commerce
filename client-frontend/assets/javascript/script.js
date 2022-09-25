7525ab943e69e4f68f1b4b3aaff7ca6ccca96d3b
jessymatta <jessy.w.matta@gmail.com>
2022-09-25 15:26:29 +0300

    EC-131 frontend: saving the edited values in the profile modal to the database

--- client-frontend/assets/javascript/script.js
+++ client-frontend/assets/javascript/script.js
@@ -3,7 +3,20 @@ const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
+//Edit profile modal
+const modalBtn = document.getElementById('edit-profile-icon');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+const modalSave = document.querySelector('.edit_profile_modal_save_btn');
+const client_name_input = document.getElementById('profile-input-name');
+const client_username_input = document.getElementById('profile-input-username');
+const client_email_input = document.getElementById('profile-input-email');
+//! To remove later on
+const client_id = 1;
+//!should be const user = JSON.parse(localStorage.getItem("user"));
+//! const client_id=user.id;
 
+//For the Carousel
 nextButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
     slidesContainer.scrollLeft += slideWidth;
@@ -14,17 +27,10 @@ prevButton.addEventListener("click", () => {
     slidesContainer.scrollLeft -= slideWidth;
 });
 
-//Edit profile modal
-const modalBtn = document.getElementById('edit-profile-icon');
-const modalBg = document.querySelector('.modal-bg');
-const modalClose = document.querySelector('.modal-close');
-const modalSave = document.querySelector('.edit_profile_modal_save_btn');
-// const burgerMenu = document.querySelector('.menu');
-
 //When the button that should show the modal is clicked
 modalBtn.addEventListener('click', function () {
-
     modalBg.classList.add('bg-active');
+    getProfile();
 
 })
 
@@ -32,32 +38,35 @@ modalBtn.addEventListener('click', function () {
 modalClose.addEventListener('click', function () {
     modalBg.classList.remove('bg-active');
 })
-
+const data = new FormData();
 //When the Save button is clicked
 modalSave.addEventListener('click', function () {
     modalBg.classList.remove('bg-active');
+    data.append('client_id', 1);
+    data.append('edited_name', client_name_input.value);
+    data.append('edited_username', client_username_input.value);
+    data.append('edited_email', client_email_input.value);
+    updateClientProfile();
 })
 
+//function to save changes on the db 
+async function updateClientProfile() {
+    const response = await axios.post(
+        `http://localhost/e-commerce/ecommerce-server/api/edit_client_profile.php`, data).then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.log(error);
+        }
+        );
+}
 
-//-------------------------------------------------------------------------------------------
 // Retrieving logged in users data and displaying it in the edit profile modal
-//! To remove later on
-const client_id = 2;
-//!should be const user = JSON.parse(localStorage.getItem("user"));
-//! const client_id=user.id;
-const client_name_input=document.getElementById('profile-input-name');
-const client_username_input=document.getElementById('profile-input-username');
-const client_email_input=document.getElementById('profile-input-email');
-
-
 async function getProfile() {
     const response = await axios.get(`http://localhost/e-commerce/ecommerce-server/api/get_client_profile.php?client_id=${client_id}`).then(
         (response) => {
-            console.log(response.data);
-            console.log(response.data.data[0]);
-            client_name_input.value=response.data.data[0].client_name;
-            client_username_input.value=response.data.data[0].client_username;
-            client_email_input.value=response.data.data[0].email;
+            client_name_input.value = response.data.data[0].client_name;
+            client_username_input.value = response.data.data[0].client_username;
+            client_email_input.value = response.data.data[0].email;
         },
         (error) => {
             console.log(error)
@@ -65,5 +74,5 @@ async function getProfile() {
     )
 }
 
-// Function calls
-getProfile();
+
+
