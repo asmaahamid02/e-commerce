34d3c3c47d3a90bcc3416e0d2acdd5f96caa6895
jessymatta <jessy.w.matta@gmail.com>
2022-09-25 20:34:35 +0300

    EC-128 frontend: linking favorites page to the get_favorites api

---
+++ client-frontend/assets/javascript/api/favorites_page.js
@@ -0,0 +1,20 @@
+const cardsContainer = document.querySelector('.product_cards_container')
+// const id = JSON.parse(localStorage.getItem('user')).id
+const id=5;
+getAllFavorites()
+
+async function getAllFavorites() {
+    const response = await axios.get(
+        'http://localhost/e-commerce/ecommerce-server/api/get_favorites.php?client_id=' +
+        id
+    )
+    console.log(response);
+    console.log(response.data);
+    const data = response.data.data
+    for (const i of data) {
+        cardsContainer.appendChild(
+            createCard(i.title, i.description, i.price, 'dummy_product_pic.jpg', i.id)
+        )
+    }
+    return data
+}
