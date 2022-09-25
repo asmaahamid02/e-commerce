85c688945a327d87617be0703212e994c6aa624b
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-09-25 20:23:44 +0300

    EC-134 linking: linking add items to cart through api

---
+++ client-frontend/assets/javascript/api/add_to_cart.js
@@ -0,0 +1,29 @@
+const add_to_cart = document.querySelector('#add-to-cart')
+const clientId = JSON.parse(localStorage.getItem('user')).id
+const product_id = sessionStorage.getItem('currentProduct')
+
+const addToCart = async () => {
+  const response = await axios.get(
+    'http://localhost/e-commerce/ecommerce-server/api/add_items_to_cart.php?client_id=' +
+      clientId +
+      '&product_id=' +
+      product_id
+  )
+  const res = response.data
+  if (res.status) {
+    triggerAlert(res.message)
+    setTimeout(function () {
+      window.location.href = './checkout.html'
+    }, 1500)
+  } else {
+    triggerAlert(res.message)
+    setTimeout(function () {
+      window.location.reload()
+    }, 1500)
+  }
+}
+
+add_to_cart.addEventListener('click', () => {
+  console.log('clicked')
+  addToCart()
+})
