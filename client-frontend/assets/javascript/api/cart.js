a04671beef462ef06830e3bc9c91f569592f48b6
asmaahamid02 <112477870+asmaahamid02@users.noreply.github.com>
2022-09-26 16:20:14 +0300

    emptying the cart before adding to it

--- client-frontend/assets/javascript/api/cart.js
+++ client-frontend/assets/javascript/api/cart.js
@@ -8,7 +8,6 @@ const get_cartItems_api =
 
 const checkout_total = document.querySelector('.checkout-total')
 const checkout_subtotal = document.querySelector('.checkout-subtotal')
-console.log(cart_container)
 
 const header = `<div class="pagetitle">
 <h1>MY BAG</h1>
@@ -18,12 +17,12 @@ let total = 0
 const getCartItems = async () => {
   const response = await axios.get(get_cartItems_api)
   const data = response.data
-  console.log(response.data)
   if (data.status == 1 && data.data != null) {
     // console.log(data)
 
     for (const product of data.data) {
       total += product.price * product.quantity
+      cart_container.innerHTML = ''
       const row = `<div class="saved-product">
       <div class="saved-product-img">
           <img src="../seller-frontend/assets/images/products/${product.image}">
